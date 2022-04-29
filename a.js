220b38555455443f17afbe262594047dd6cd3adf
WS021227 <2812339731@qq.com>
2022-04-29 10:54:40 +0800

    李四改好了

--- a.js
+++ a.js
@@ -5,4 +5,14 @@ console.log("a3")
 //使用git clone git@github.com:WS021227/lenrgit.git 来下载gti的代码
 //自己添加修改后，git add *,git commit -m "提示"
 //先git pull 获取最新数据，再git push 上传更新的数据
+
+
+//另外一个人，每天第一件事，使用git pull获取最新的数据，因为别人可能修改过了
+
+
+
+
+//代码冲突问题
 console.log("张三1")
+
+console.log("i am lisi")
