cefc31dc6c6d7095a930869f34b683c73f8b2b54
WS021227 <2812339731@qq.com>
2022-04-29 10:44:37 +0800

    张三添加

--- a.js
+++ a.js
@@ -2,5 +2,7 @@ console.log("a1")
 console.log("a2")
 console.log("a3")
 
-
-console.log("张三")
+//使用git clone git@github.com:WS021227/lenrgit.git 来下载gti的代码
+//自己添加修改后，git add *,git commit -m "提示"
+//先git pull 获取最新数据，再git push 上传更新的数据
+console.log("张三1")
